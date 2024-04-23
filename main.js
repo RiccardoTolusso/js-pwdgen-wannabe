423ebf9806e2b5931a6cde4c48e3772a1833cb1d
RiccardoTolusso <riccardotolusso215@gmail.com>
2024-04-24 00:52:51 +0200

    [END OF EXERCISE] for the bonus i created 2 versions one with Math.random and one with datetime wich took a little bit more effort even though the math random should be more efficient im stilll prud of my secondary solution

--- main.js
+++ main.js
@@ -8,6 +8,19 @@ firstName = prompt(message="Nome:");
 lastName = prompt(message="Cognome:");
 favColour = prompt(message="Colore prefertito:");
 
+// valore per scegliere la soluzione da adottare
+let solution = 2; 
+
+switch (solution){
+    case 1:
+        randomNumber = Math.floor(Math.random()*100);
+        break;
+
+    case 2:
+        randomNumber = parseInt(String(new Date().getTime()).slice(-2))
+        break;
+}
+
 document.getElementById("result").innerHTML = firstName+lastName+favColour+randomNumber;
 
 console.log(`
